4d0bb9151f5866dfa28be8702039659a56140ec1
LAPTOP-DK <qw5057@naver.com>
2023-05-30 15:55:06 +0900

    008. Avalonia UI
    Add Text Binding (MVVM)

--- StudyAvaloniaUI/ViewModels/MainWindowViewModel.cs
+++ StudyAvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,20 @@
-﻿namespace StudyAvaloniaUI.ViewModels;
+﻿using ReactiveUI;
+
+namespace StudyAvaloniaUI.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private string _boldTitle = "AVALONIA";
+    public string BoldTitle
+    {
+        get => _boldTitle;
+        set => this.RaiseAndSetIfChanged(ref _boldTitle, value);
+    }
+    
+    private string _regularTitle = "LOUDNESS METER";
+    public string RegularTitle
+    {
+        get => _regularTitle;
+        set => this.RaiseAndSetIfChanged(ref _regularTitle, value);
+    }
 }
